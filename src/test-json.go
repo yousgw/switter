e25c08ba734e9981262476ac34990bc100d9b8eb
you86 <r115322@g.kobe-kosen.ac.jp>
2020-10-19 23:09:33 +0900

    jsonファイルの作成。追加書き込み可能に。

--- src/test-json.go
+++ src/test-json.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
 	"time"
 	"fmt"
 )
@@ -12,8 +15,29 @@ type Data struct {
 
 func main(){
 	var data1 = Data{
-		time.Now(),"test1",
+		time.Now(),"",
 	}
-	fmt.Println(data1.Time)
-	fmt.Println(data1.Detail)
+	fmt.Print("data input =>")
+	fmt.Scan(&data1.Detail)
+	fmt.Print("\n\n")
+
+	b,err := json.Marshal(data1)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	file,err:=os.OpenFile("./test.json",os.O_APPEND|os.O_CREATE,0600)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data1)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(b))
+	//fmt.Println(data1.Time)
+	//fmt.Println(data1.Detail)
 }
