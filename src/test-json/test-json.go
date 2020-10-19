c1e4f411b1b43c7e2ef573cec41cc62e75f97fb4
yousgw <r115322@g.kobe-kosen.ac.jp>
2020-10-19 23:20:11 +0900

    ファイル整理。test-jsonの整形。

--- src/test-json/test-json.go
+++ src/test-json/test-json.go
@@ -2,38 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"time"
-	"fmt"
 )
 
 type Data struct {
-	Time time.Time
+	Time   time.Time
 	Detail string
 }
 
-func main(){
+func main() {
 	var data1 = Data{
-		time.Now(),"",
+		time.Now(), "",
 	}
 	fmt.Print("data input =>")
 	fmt.Scan(&data1.Detail)
 	fmt.Print("\n\n")
 
-	b,err := json.Marshal(data1)
-	if err != nil{
+	b, err := json.Marshal(data1)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	file,err:=os.OpenFile("./test.json",os.O_APPEND|os.O_CREATE,0600)
-	if err != nil{
+	file, err := os.OpenFile("./test.json", os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data1)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
