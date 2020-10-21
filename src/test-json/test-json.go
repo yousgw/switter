6aa8390e79a7c9552f1a287ee5ec17488a01c81d
yousgw <r115322@g.kobe-kosen.ac.jp>
2020-10-21 23:35:58 +0900

    jsonファイルを1行ごとに読み出し、標準出力

--- src/test-json/test-json.go
+++ src/test-json/test-json.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -21,11 +22,18 @@ func main() {
 	fmt.Scan(&data1.Detail)
 	fmt.Print("\n\n")
 
-	b, err := json.Marshal(data1)
+	_, err := json.Marshal(data1)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//fmt.Println(data1)
+
+	//var data3 Data
+	//json.Unmarshal(jdata,&data3)
+
+	//書き込みテスト
+	/*
 	file, err := os.OpenFile("./test.json", os.O_APPEND|os.O_CREATE, 0600)
 	if err != nil {
 		log.Fatal(err)
@@ -36,8 +44,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	file.Close()
+	*/
 
-	fmt.Println(string(b))
-	//fmt.Println(data1.Time)
-	//fmt.Println(data1.Detail)
+	//読み出しテスト
+
+	var data2 Data
+
+	datas, err := os.Open("./test.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	string := bufio.NewScanner(datas)
+
+	for i :=1;string.Scan();i++{
+		//fmt.Println(string.Text())
+		json.Unmarshal([]byte(string.Text()),&data2)
+		fmt.Println(data2.Time.Year(),"年",data2.Time.Month(),"月",data2.Time.Day(),"日",data2.Time.Hour(),":",data2.Time.Minute(),":",data2.Time.Second())
+		fmt.Println(data2.Detail,"\n")
+	}
+
+
 }
